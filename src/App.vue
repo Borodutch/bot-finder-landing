5fe9ad2c49f62e9aab9641baa022cd089c3d934b
backmeupplz <backmeupplz@gmail.com>
2021-03-25 09:06:09 -0700

    update

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,34 @@ v-app
   v-main
     .v-container.pa-4
       v-layout.text-center(column, justify-center, align-center)
-        h1 VC Это бот?
-        subtitle-1 Простое расширение, которое позволяет быстро увидеть, бот ли пользователь в комментариях.
-        subtitle-1 Показывает важную информацию типа даты регистрации пользователя и количества комментариев.
-        body-1.mt-4
-          | {{ "Скачать для " }}
-          a(href='google.com') Хрома
-          | {{ ", " }}
-          a(href='google.com') Фаерфокса
-          | {{ ", " }}
-          a(href='google.com') Сафари
-          | {{ ", " }}
-          a(href='google.com') Оперы
+        .text-h1 VC Это бот?
+        .text-subtitle-1 Простое расширение, которое позволяет быстро увидеть, бот ли пользователь в комментариях.
+        .text-subtitle-1 Показывает важную информацию типа даты регистрации пользователя и количества комментариев.
+        .text-subtitle-1
+          | {{ "Код расширения " }}
+          a(
+            target='_blank',
+            href='https://github.com/backmeupplz/vc-bots-finder'
+          ) открыт
+          | {{ ". Код этого сайта " }}
+          a(
+            target='_blank',
+            href='https://github.com/backmeupplz/vc-bots-finder-landing'
+          ) тоже
           | .
-        body-1.mt-8
+        .text-body-1.mt-4
+          | {{ "Скачать для " }}
+          a(target='_blank', href='google.com') Хрома
+          | {{ ", " }}
+          a(target='_blank', href='google.com') Фаерфокса
+          | {{ ", " }}
+          a(target='_blank', href='google.com') Сафари
+          | {{ ", " }}
+          a(target='_blank', href='google.com') Оперы
+          | .
+        .text-body-1.mt-8
           a(href='https://terms.borodutch.com', target='_blank') Условия использования
-        body-1
+        .text-body-1
           a(href='https://privacy.borodutch.com', target='_blank') Политика конфиденциальности
 </template>
 
