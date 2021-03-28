add52ac90fd200adec18ccfa79c457effc1ed8df
backmeupplz <backmeupplz@gmail.com>
2021-03-28 07:33:52 -0700

    update

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,22 @@ v-app
     .v-container.pa-4
       v-layout.text-center(column, justify-center, align-center)
         .text-h1 Это бот?
-        .text-subtitle-1 Простое расширение, которое позволяет быстро увидеть, бот ли пользователь в комментариях на vc.ru, TJournal и DTF.
+        .text-subtitle-1
+          | {{ "Простое расширение, которое позволяет быстро увидеть, бот ли пользователь в комментариях на " }}
+          a(target='_blank', href='https://vc.ru') vc.ru
+          | {{ ", " }}
+          a(target='_blank', href='https://tjournal.ru') TJournal
+          | {{ " и " }}
+          a(target='_blank', href='https://dtf.ru') DTF
+          | .
         .text-subtitle-1 Показывает важную информацию типа даты регистрации пользователя и количества комментариев.
         .text-subtitle-1
           | {{ "Код расширения " }}
-          a(
-            target='_blank',
-            href='https://github.com/backmeupplz/vc-bots-finder'
-          ) открыт
+          a(target='_blank', href='https://github.com/backmeupplz/bot-finder') открыт
           | {{ ". Код этого сайта " }}
           a(
             target='_blank',
-            href='https://github.com/backmeupplz/vc-bots-finder-landing'
+            href='https://github.com/backmeupplz/bot-finder-landing'
           ) тоже
           | .
         .text-body-1.mt-4
@@ -27,7 +31,7 @@ v-app
           | {{ " и " }}
           a(
             target='_blank',
-            href='https://addons.mozilla.org/en-US/firefox/addon/vc-is-this-a-bot/'
+            href='https://addons.mozilla.org/en-US/firefox/addon/bot-finder/'
           ) Фаерфокса
           | .
         .text-body-1.mt-8
